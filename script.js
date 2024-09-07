a5158d7c223326c7c0a5e2750da9361656f4e4df
ostapsivko <o-stap@hotmail.com>
2024-09-08 00:12:51 +0300

    Add board shaking

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-let divs = new Array(16);
 let container = document.querySelector(".container");
+let grid = createGrid(4);
+grid.forEach(element => {
+    container.appendChild(element);
+});
 
-for(i = 0; i < 16; i++) {
-    let div = document.createElement("div");
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "black";
+let shaker = document.querySelector(".shaker");
+shaker.addEventListener("click", () => {
+    let sizeInput = prompt("Please enter grid size up to 100", 10);
+    let size = Number(sizeInput)
+    if(size > 100) {
+        size = 100;
+    }
+
+    if(size < 0) {
+        size = 1;
+    }
+
+    let newGrid = createGrid(size);
+    container.replaceChildren();
+    newGrid.forEach(element => {
+        container.appendChild(element);
     });
-    divs[i] = div;
-    container.appendChild(div);
+});
+
+function createGrid(size) {
+    let divs = new Array(size);
+
+    for(i = 0; i < size*size; i++) {
+        let div = document.createElement("div");
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "black";
+        });
+        divs[i] = div;
+    }
+
+    return divs;
 }
